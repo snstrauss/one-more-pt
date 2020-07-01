93042cd926b51decf559c0a5fbf533870135a68d
snstrauss <zahavi.guy.85@gmail.com>
2020-07-01 14:41:55 +0300

    generating launce circle data on init

--- src/services/game.service.js
+++ src/services/game.service.js
@@ -1,10 +1,23 @@
 import { createCircle } from "./body.service";
+import { space } from "./world.service";
 
 const INITIAL_PTS_COUNT = 5;
 
 export const allCircles = [];
-
+export let launchCircle;
 export function gameInit(){
+
+    const launchCircleRadius = 80;
+
+    launchCircle = createCircle(0, launchCircleRadius, {
+        position: {
+            x: space.size.x / 2,
+            y: space.size.y,
+        },
+        color: 'red'
+    });
+    launchCircle.width = 1;
+    launchCircle.radius = launchCircleRadius;
 
     for(let i = INITIAL_PTS_COUNT; i > 0; i--){
 
